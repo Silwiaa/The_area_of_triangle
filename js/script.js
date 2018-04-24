82b4ca0b53aed1a894e40f0f88e67c6fe43fefd0
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-04-24 20:55:10 +0200

    Upload js/script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// The area of triangle
+
+var a = prompt('Provide the a value (base) of triangle'),
+    h = prompt('Provide the h value (height) of triangle'),
+    triangleArea = a * h / 2;
+
+alert('Your triangle area is: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
